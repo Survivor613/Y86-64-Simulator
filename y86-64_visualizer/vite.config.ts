bc878e64dd92c0973d510ab5acf11038c1537c49
Survivor613 <fantingshuo@qq.com>
2025-11-28 10:38:00 +0800

    visualization repair

--- y86-64_visualizer/vite.config.ts
+++ y86-64_visualizer/vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Y86-64-Simulator/',    // Github Pages 必备
+
       server: {
         port: 3000,
         host: '0.0.0.0',
